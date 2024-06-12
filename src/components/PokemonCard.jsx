6b7e5c4503164fbc46174b3f55feac9e7ac6ff8e
Rosa Benchabane <benchabane.rosa@outlook.com>
2024-06-12 18:18:47 +0200

    pokedex 4 - props1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,7 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-export default function PokemonCard (){
-    const pokemon = pokemonList[0];
+export default function PokemonCard ({ pokemon }){
+    console.log(pokemon);
+    // const pokemon = pokemonList[0];
+    // const pokemon = { name: "mew" };
 // if imgSrc exists in pokemonList then add img with src and alt otherwise add p with "?????" 
     return  <figure>{pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>?????</p>)}<figcaption>{pokemon.name}</figcaption></figure>;
 
