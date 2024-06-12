7858e1d4f58b42e8ba8ef0c90a348a8843045d20
Rosa Benchabane <benchabane.rosa@outlook.com>
2024-06-12 17:29:05 +0200

    pokedex 1

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,5 @@
+export default function PokemonCard (){
+    return  <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon Bulbasaur"/><figcaption>Bulbasaur</figcaption></figure>;
+
+
+}
