0cf5ce430be1108af96efd232eabb5d600a9b5e8
Rosa Benchabane <benchabane.rosa@outlook.com>
2024-06-13 16:19:20 +0200

    pokedex - state

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,60 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard"
 
+import './App.css'
 
 const pokemonList = [
   {
-    // name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 
 function App() {
+
+
+  const [pokemonIndex, setIndex] = useState(0);
+
+  //  si pokemonIndex est > 0, donc retire  1 à set Index de pokemonIndex sinon mets la taille du tableau -1 (index commence par 0 donc la longueur doit deduire 1 pour tomber sur le numéro correct)
+  const handlePrev = () => {
+    setIndex(pokemonIndex > 0 ? pokemonIndex - 1 : pokemonList.length - 1);
+  }
+//  si pokemonIndex est < la taille du tableau -1 (index commence par 0 donc la longueur doit deduire 1 pour tomber sur le numéro correct), donc rajoute  1 à set Index de pokemonIndex sinon mets zéro pour repartir de index 0 
+  const handleNext = () => {
+    setIndex(pokemonIndex < pokemonList.length - 1 ? pokemonIndex + 1 : 0);
+  }
+
   
   return (
-    <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
-      <PokemonCard pokemon={pokemonList[1]} />
+<section>
+<div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
     </div>
+    <div className="nav-buttons">
+      <button onClick={handlePrev}>Précédent</button>
+      <button onClick={handleNext}>Suivant</button>
+    </div>
+</section>
   )
 
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
 
+
 export default function PokemonCard({ pokemon }) {
-    console.log(pokemon);
+    // console.log(pokemon);
     // const pokemon = pokemonList[0];
     // const pokemon = { name: "mew" };
+
 
     // If imgSrc exists in pokemon then add img with src and alt, otherwise add p with "?????"
     return (
